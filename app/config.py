f4b09ef58146b72ee3782068fd2b576a50a1784a
Danny Haro <haro82507@gmail.com>
2025-06-18 14:14:07 -0500

    subiendo a rende

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/dbsgce'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://sgce_database_user:zsdrqGq9lICjbszmxl9g3c8RB2s9jaMB@dpg-d19ff8re5dus738u4g5g-a.oregon-postgres.render.com/sgce_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('YmlnZGF0YTIwMjU=') or 'YmlnZGF0YTIwMjU='
     FLASK_ENV = 'development'
