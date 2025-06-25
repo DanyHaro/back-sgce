2c5ef6e86a7145d55a2854de9ceb30c1b7647588
Danny Haro <haro82507@gmail.com>
2025-06-25 18:26:49 -0500

    add api get rubrica

--- app/blueprints/dashboard_principal/resultado.py
+++ app/blueprints/dashboard_principal/resultado.py
@@ -1,7 +1,20 @@
-#from flask import Blueprint, jsonify, request
-#from ...models import ResultadoRubrica, db,Sesion, Rubrica
+from flask import Blueprint, jsonify, request
+from ...models import ResultadoRubrica, db,Sesion, Rubrica
 
-#main_routes = Blueprint('resultados_rubrica', __name__)
+main_routes = Blueprint('resultados_rubrica', __name__)
 
+@main_routes.route('/rubrica', methods=['GET'])
+def list_rubricas():
+    rubricas = Rubrica.query.all()
 
+    resultado = []
+    for r in rubricas:
+        resultado.append({
+            'id': r.id,
+            'factores': r.factores,
+            'criterios': r.criterios,
+            'items': r.items
+        })
+
+    return jsonify({'rubricas': resultado}), 200
 
