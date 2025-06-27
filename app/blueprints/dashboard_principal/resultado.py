3dd87fee0b8a08a37344b332ed9e909a9d06dcd5
Danny Haro <haro82507@gmail.com>
2025-06-26 22:11:48 -0500

    arreglando json get de rubrica

--- app/blueprints/dashboard_principal/resultado.py
+++ app/blueprints/dashboard_principal/resultado.py
@@ -4,17 +4,35 @@ from ...models import ResultadoRubrica, db,Sesion, Rubrica
 main_routes = Blueprint('resultados_rubrica', __name__)
 
 @main_routes.route('/rubrica', methods=['GET'])
-def list_rubricas():
+def get_rubricas():
     rubricas = Rubrica.query.all()
+    factores = []
+    
+    # Agrupamos los resultados por factores
+    for rubrica in rubricas:
+        factor_exist = next((factor for factor in factores if factor['nombre'] == rubrica.factores), None)
+        
+        if not factor_exist:
+            factor_exist = {
+                'nombre': rubrica.factores,
+                'criterios': []
+            }
+            factores.append(factor_exist)
+        
+        # Agregar los criterios dentro del factor
+        criterio = {
+            'nombre': rubrica.criterios,
+            'itemsEvaluacion': []
+        }
+        
+        # Desglosar los items
+        items = rubrica.items.split('\n')
+        for i, item in enumerate(items, start=1):
+            criterio['itemsEvaluacion'].append({
+                'id': i,
+                'nombre': item.strip()
+            })
+        
+        factor_exist['criterios'].append(criterio)
 
-    resultado = []
-    for r in rubricas:
-        resultado.append({
-            'id': r.id,
-            'factores': r.factores,
-            'criterios': r.criterios,
-            'items': r.items
-        })
-
-    return jsonify({'rubricas': resultado}), 200
-
+    return jsonify({"factores": factores})
